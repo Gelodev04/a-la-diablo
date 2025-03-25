1a8e4b954d1f57ee04cbec3218c1623aeb0155a7
gelo <gelodev83@gmail.com>
2025-03-25 10:27:27 +0800

    updated

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -3,10 +3,17 @@ import FormInput from "./FormInput";
 export default function ComingSoon() {
   return (
     <section className="text-white w-3/4 ">
-      <h1 className="font-bold text-[5rem] font-pirata leading-tight">
+      {/* <h1 className="font-bold text-[5rem] font-pirata leading-tight">
         We are Coming Soon!
+      </h1> */}
+        <h1 
+        className="font-bold text-[5rem] font-pirata leading-tight 
+                   bg-gradient-to-r from-orange-500 via-[#ff1e00] to-orange-600
+                   text-transparent bg-clip-text animate-gradient"
+      >
+        We are <span className="">Coming</span> Soon!
       </h1>
-      <p className="font-inter mb-5 ">Sign up to be the first to know when we launch!</p>
+      <p className="font-inter mb-5 text-[15px]">Sign up to be the first to know when we launch!</p>
       <FormInput />
       
     </section>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export const Navbar = () => {
   return (
     <header className='absolute w-full '>
         <nav className='flex items-center w-full justify-between py-10 px-16 font-pirata text-white'>
-            <h1 className='text-[2.5rem]'>A La Diablo <span className='text-[#ff1e00]'>Hot Sauce</span></h1>
+            <h1 className='text-[2.5rem]'>A La Diabla <span className='text-[#ff1e00]'>Hot Sauce</span></h1>
             <SocialMedia/>
         </nav>
     </header>
