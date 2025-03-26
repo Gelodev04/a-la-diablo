2989944e764bbebd5bb3bd0b89d0aac9d5766f0e
gelo <gelodev83@gmail.com>
2025-03-26 23:43:00 +0800

    updated

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -4,7 +4,7 @@ import { TopEdge, BottomEdge } from "./ui/Edge";
 export default function ComingSoon() {
   return (
     <section className="text-white  relative group mx-auto max-w-[700px]     px-5 py-1 group">
-       <div className="absolute inset-0 flex justify-between  z-[-1] ">
+      <div className="absolute inset-0 flex justify-between  z-[-1] ">
         <div className="top-0 left-0 ">
           <TopEdge />
         </div>
@@ -14,19 +14,18 @@ export default function ComingSoon() {
       </div>
 
       <h1
-        className="font-bold text-[3.5rem] sm:text-[4rem] md:text-[4.5rem] lg:text-[5rem] xl:text-[6rem]   font-pirata leading-tight 
-                   bg-gradient-to-tr from-[#ff4500] via-[#ff1e00] to-[#ffcc00]
-                   text-transparent bg-clip-text animate-gradient drop-shadow-[2px_2px_7px_rgba(255,30,0,0.8)] tracking-wider "              
+        className="font-bold text-[3.5rem] sm:text-[4rem] md:text-[4.5rem] lg:text-[5rem] xl:text-[6rem] font-pirata leading-tight 
+             bg-gradient-to-tr from-[#0d741b] via-[#085a14] to-[#064010]
+             text-transparent bg-clip-text animate-gradient drop-shadow-[2px_2px_7px_rgba(13,116,27,0.8)] tracking-wider "
       >
         We are <span className="">Coming</span> Soon!
       </h1>
+
       <p className="font-montserrat mb-5 text-[15px] ">
         Sign up to be the first to know when we launch!
       </p>
-      
-        <FormInput />
-      
-     
+
+      <FormInput />
     </section>
   );
 }
